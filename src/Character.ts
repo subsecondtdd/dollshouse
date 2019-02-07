64ed59a307b210e574cbad584c7fae49f7cc4ff9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2019-02-07 21:39:40 +0000

    Add memory

--- src/Character.ts
+++ src/Character.ts
@@ -1,12 +1,43 @@
 export default class Character<UserInfo = {}, UserAgent = {}> {
+  private readonly memory = new Map<any, any>()
   private _userInfo: UserInfo
   private userAgent: UserAgent
 
   constructor(private readonly name: string, private readonly makeUserAgent: (userInfo: UserInfo) => Promise<UserAgent>) {
   }
 
+  /**
+   * Sets user info. This will be passed to the {@link makeUserAgent} function.
+   * This is typically used to asuthenticate the character's user agent.
+   *
+   * @param userInfo
+   */
   set userInfo(userInfo: UserInfo) {
     this._userInfo = userInfo
+  }
+
+  /**
+   * Remember something
+   *
+   * @param key the name of the thing to remember
+   * @param value what to remember
+   */
+  public remember<T>(key: any, value: T) {
+    this.memory.set(key, value)
+  }
+
+  /**
+   * Recall something previously remembered
+   *
+   * @param key the name of the thing to recall
+   * @return the value that was recalled
+   * @throws Error if nothing can be recalled.
+   */
+  public recall<T>(key: any): T {
+    if (!this.memory.has(key)) {
+      throw new Error(`${this.name} does not recall anything about ${key}`)
+    }
+    return this.memory.get(key)
   }
 
   async attemptsTo(action: (userAgent: UserAgent) => Promise<UserAgent>) {
